a901e0ef6e919bb25faf39136a6d48baf4682979
cz303 <56400025+cz303@users.noreply.github.com>
2019-10-20 02:00:03 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-module.exports = {telegraf_token:'your telegraf token',
+module.exports = {telegraf_token:'840666289:AAE4VfAVzmCYdqOpsrZhMM8rxJ7al-l1rJk',
 database:'Your database connection string',
 secret:'yoursecret'};
-
